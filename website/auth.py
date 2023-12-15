8e9b4fbd3c89704b7ba595ef2794a15ca55eb691
Vector-013 <vectorprime1397@gmail.com>
2023-12-15 16:24:38 +0530

    added functionality of user sign up

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -33,7 +36,11 @@ def sign_up():
         elif password1 != password2:
             flash("Passwords dont match", category="error")
         else:
-            #add user to database
+            
+            new_user = User(email = email, password=generate_password_hash(password1), first_name = firstName)
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account Created!", category="success")
+            return redirect(url_for("views.home"))
     
     return render_template("sign_up.html")
